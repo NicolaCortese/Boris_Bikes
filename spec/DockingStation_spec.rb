7d87b3a40641f7e1cccfe7ff0c5a965b920e9ed3
PhilBligh <philbligh@hotmail.com>
2022-02-22 16:19:00 +0000

    step 9 completed

--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -1,4 +1,6 @@
 require 'dockingstation'
 describe DockingStation do
-    
+  it "Expects Docking Station instances to respond to release_bike" do
+    expect(subject).to respond_to('release_bike') 
+  end
 end

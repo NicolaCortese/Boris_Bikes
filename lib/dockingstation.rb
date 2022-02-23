7ac8e112f6eb91cd277ef06cfb615b0385cef82b
Halwoll <97443146+Halwoll@users.noreply.github.com>
2022-02-23 15:07:51 +0000

    question11

--- lib/dockingstation.rb
+++ lib/dockingstation.rb
@@ -1,6 +1,18 @@
-require 'bike'
+require_relative 'bike'
 class DockingStation
+  def initialize
+    @bikes = []
+  end
+
+  attr_reader :docked_bikes
+  docked_bikes = @bikes
+  
   def release_bike
     Bike.new
   end
+
+  def dock(bike)
+    @bikes.push(bike)
+  end
+
 end
--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -1,11 +1,19 @@
 require 'dockingstation'
+
 describe DockingStation do
   it "Expects Docking Station instances to respond to release_bike" do
     expect(subject).to respond_to('release_bike') 
   end
+
   it "Release bike to produce instance of bike class and expects bike to be working" do
     bike = subject.release_bike
     expect(bike).to be_working
     #expect(bike).working?.to eq true
   end
+
+  it ".dock method adds a bike to docking station" do
+    bike = Bike.new
+    expect(subject.dock(bike)).to include(bike)
+  end
+
 end
